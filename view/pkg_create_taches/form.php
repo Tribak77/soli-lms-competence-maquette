4447637df6eea57b83a00b0535a08b1df290824e
Suirita Fahd <152966939+Suirita@users.noreply.github.com>
2024-11-08 20:26:03 +0100

    initial commit

---
+++ view/pkg_create_taches/form.php
@@ -0,0 +1,29 @@
+<form>
+  <div class="form-group">
+    <label for="tacheNom">Nom de tache</label>
+    <input type="text" class="form-control" id="tacheNom" placeholder="Enter role name" value="Tuto-3">
+  </div>
+  <div class="form-group">
+    <label for="tacheDescription">Description</label>
+    <input type="text" class="form-control" id="tacheDescription" placeholder="Enter role name" value="realiser le tutorial 3">
+  </div>
+  <div class="form-group">
+    <label for="skill">Project</label>
+    <select class="form-control" id="skill">
+      <option value="CC1">Application de gestion de bibliotheque</option>
+      <option value="CC2">une application galerie d'art</option>
+      <option value="CC3">une base de donnees pour un blog</option>
+      <option value="CC4">une application My City</option>
+      <option value="CC5">boutique en ligne simplifiee</option>
+      <option value="CC6">une application de recherche de vois</option>
+      <option value="CC7">un project avec un design personnalise</option>
+    </select>
+  </div>
+  
+  <div class="float-sm-right mr-2">
+    <a href="./index.php" class="btn btn-info">
+      Confirmer
+    </a>
+  </div>
+
+</form>
